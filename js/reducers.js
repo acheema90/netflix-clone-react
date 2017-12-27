5a99475b30f60b8593c8f77210df42f9da682fbf
Ajaypal Cheema <ajaypal.cheema@sgainc.com>
2017-12-27 16:50:13 -0500

    adding redux

---
+++ js/reducers.js
@@ -0,0 +1,12 @@
+const DEFAULT_STATE = {
+    searchTerm: ''
+};
+
+const rootReducer = (state = DEFAULT_STATE, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default rootReducer;
