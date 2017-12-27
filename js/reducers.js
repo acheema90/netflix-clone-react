e847f1d49003cf65d90d75301082f941f086e23e
Ajaypal Cheema <ajaypal.cheema@sgainc.com>
2017-12-27 17:07:49 -0500

    update reducers

--- js/reducers.js
+++ js/reducers.js
@@ -1,9 +1,19 @@
+import { SET_SEARCH_TERM } from './actions';
+
 const DEFAULT_STATE = {
     searchTerm: ''
 };
 
+const setSearchTerm = (state, action) => {
+    // must retun new state
+    // so we return a new obejct with old state and the new searchTerm
+    return Object.assign({}, state, {searchTerm: action.payload });
+};
+
 const rootReducer = (state = DEFAULT_STATE, action) => {
     switch (action.type) {
+        case SET_SEARCH_TERM:
+            return setSearchTerm(state, action);
         default:
             return state;
     }
