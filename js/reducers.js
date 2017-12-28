cc105a115faba130b2276e3aa33c4909ffc3dd18
Ajaypal Cheema <ajaypal.cheema@sgainc.com>
2017-12-28 15:43:48 -0500

    use combineReducers
    refactor reducers to use combineReducers from redux

--- js/reducers.js
+++ js/reducers.js
@@ -1,22 +1,13 @@
+import { combineReducers } from 'redux';
 import { SET_SEARCH_TERM } from './actions';
 
-const DEFAULT_STATE = {
-    searchTerm: ''
-};
-
-const setSearchTerm = (state, action) => {
-    // must return new state
-    // so we return a new object with old state and the new searchTerm
-    return Object.assign({}, state, {searchTerm: action.payload });
-};
-
-const rootReducer = (state = DEFAULT_STATE, action) => {
-    switch (action.type) {
-        case SET_SEARCH_TERM:
-            return setSearchTerm(state, action);
-        default:
-            return state;
+const setSearchTerm = (state = '', action) => {
+    if (action.type === SET_SEARCH_TERM) {
+        return action.payload;
     }
+    return state;
 };
+
+const rootReducer = combineReducers({searchTerm: setSearchTerm});
 
 export default rootReducer;
