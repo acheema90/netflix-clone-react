146e8cae76d9350ea8a20c3edaf2799cd3e7e5c4
Ajaypal Cheema <ajaypal.cheema@sgainc.com>
2018-01-26 20:17:53 -0500

    add server-side rendering

--- js/App.jsx
+++ js/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import Landing from './Landing';
@@ -10,24 +10,22 @@ import preload from '../data';
 const FourOhFour = () => <h1>404</h1>;
 
 const App = () => (
-    <BrowserRouter>
-        <Provider store={store}>
-            <div className='app'>
-                <Switch>
-                    <Route exact path='/' component={(props) => <Landing shows={preload.shows} {...props} />} />
-                    <Route path='/search' component={(props) => <Search shows={preload.shows} {...props}/> } />
-                    <Route
-                        path='/details/:id'
-                        component={(props) => {
-                            const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
-                            return <Details show={selectedShow} {...props} />}
-                        }
-                    />
-                    <Route component={FourOhFour} />
-                </Switch>
-            </div>
-        </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+        <div className='app'>
+            <Switch>
+                <Route exact path='/' component={(props) => <Landing shows={preload.shows} {...props} />} />
+                <Route path='/search' component={(props) => <Search shows={preload.shows} {...props}/> } />
+                <Route
+                    path='/details/:id'
+                    component={(props) => {
+                        const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
+                        return <Details show={selectedShow} {...props} />}
+                    }
+                />
+                <Route component={FourOhFour} />
+            </Switch>
+        </div>
+    </Provider>
 );
 
 export default App;
